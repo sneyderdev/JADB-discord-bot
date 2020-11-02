afb45007326c477da8d590d49ff2c7dfdf9d0840
Sneyder Barreto <sneyderbarreto@gmail.com>
2020-11-02 18:55:01 -0500

    ADD: Basic command handler

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const command = require('./src/commands/command');
+
 client.once('ready', () => {
   console.log('The client is ready!');
+
+  command(client, ['ping', 'test'], (message) => {
+    message.channel.send('Pong!');
+  });
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
