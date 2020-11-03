254d658a7f02a9a9be2a7e47acd91aeb57c46b89
Sneyder Barreto <sneyderbarreto@gmail.com>
2020-11-02 19:27:08 -0500

    ADD: New commands (servers, clearchannel, status)

--- index.js
+++ index.js
@@ -8,8 +8,41 @@ const command = require('./src/commands/command');
 client.once('ready', () => {
   console.log('The client is ready!');
 
+  // Basic command
   command(client, ['ping', 'test'], (message) => {
     message.channel.send('Pong!');
+  });
+
+  // List servers command
+  command(client, 'servers', (message) => {
+    client.guilds.cache.forEach((guild) => {
+      message.channel.send(
+        `${guild.name} has a total of ${guild.memberCount} members`
+      );
+    });
+  });
+
+  // Clear channel command
+  command(client, ['cc', 'clearchannel'], (message) => {
+    if (message.member.hasPermission('ADMINISTRATOR')) {
+      message.channel.messages.fetch().then((results) => {
+        message.channel.bulkDelete(results);
+      });
+    } else {
+      message.reply("you don't have permissions for using this command.");
+    }
+  });
+
+  // Set status command
+  command(client, 'status', (message) => {
+    const content = message.content.replace('!status', '');
+
+    client.user.setPresence({
+      activity: {
+        name: content,
+        type: 0,
+      },
+    });
   });
 });
 
