ece092367d77f3aa5bcce4b75304fff2597225f1
Sneyder Barreto <sneyderbarreto@gmail.com>
2020-11-03 23:04:51 -0500

    ADD: New commands (embed, serverinfo, help)

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const config = require('./src/data/config.json');
 const command = require('./src/commands/command');
 const firstMessage = require('./src/utils/firstMessage');
 const privateMessage = require('./src/utils/privateMessage');
@@ -90,6 +91,124 @@ client.once('ready', () => {
         channel.setParent(categoryId);
         channel.setUserLimit(10);
       });
+  });
+
+  // Embed command
+  command(client, 'embed', (message) => {
+    const gif = 'https://media.giphy.com/media/Id0IZ49MNMzKHI9qpV/giphy.gif';
+
+    const embed = new Discord.MessageEmbed()
+      .setTitle('Follow me on GitHub!')
+      .setURL('https://github.com/sneyderdev')
+      .setAuthor(message.author.username)
+      .setThumbnail(gif)
+      .setImage(gif)
+      .setFooter('This is a footer', gif)
+      .setColor('#C435E8')
+      .addFields(
+        {
+          name: 'Field 1',
+          value: 'Hello world!',
+          inline: true,
+        },
+        {
+          name: 'Field 2',
+          value: 'Hello planet!',
+          inline: true,
+        },
+        {
+          name: 'Field 3',
+          value: 'Hello people!',
+          inline: true,
+        }
+      );
+
+    message.channel.send(embed);
+  });
+
+  // Server info command
+  command(client, 'serverinfo', (message) => {
+    const { guild } = message;
+    const { name, region, memberCount, owner } = guild;
+    const icon = guild.iconURL();
+
+    const embed = new Discord.MessageEmbed()
+      .setTitle(`Server info for "${name}"`)
+      .setThumbnail(icon)
+      .addFields(
+        {
+          name: 'Region',
+          value: region,
+        },
+        {
+          name: 'Member Count',
+          value: memberCount,
+        },
+        {
+          name: 'Owner',
+          value: owner.user.tag,
+        }
+      );
+
+    message.channel.send(embed);
+  });
+
+  // Help command
+  command(client, 'help', (message) => {
+    const { prefix } = config;
+    client.user.setPresence({
+      activity: {
+        name: `"${prefix}help" if you feel lost!`,
+      },
+    });
+
+    const { guild } = message;
+    const icon = guild.iconURL();
+
+    const embed = new Discord.MessageEmbed()
+      .setTitle('These are my supported commands:')
+      .setThumbnail(icon)
+      .setColor('#C435E8')
+      .addFields(
+        {
+          name: '`!ping`',
+          value: 'Play ping pong with me!',
+        },
+        {
+          name: '`!servers`',
+          value: "Lists the servers I'm in",
+        },
+        {
+          name: '`!clearchannel`',
+          value: 'Clears messages in a particular channel',
+        },
+        {
+          name: '`!status [status message]`',
+          value: 'Sets the status of the bot',
+        },
+        {
+          name: '`!createtextchannel [channel name]`',
+          value: 'Creates a new text channel',
+        },
+        {
+          name: '`!createvoicechannel [channel name]`',
+          value: 'Creates a new voice channel',
+        },
+        {
+          name: '`!embed`',
+          value: 'Creates an embed message',
+        },
+        {
+          name: '`!serverinfo`',
+          value: 'Gets information about your server',
+        },
+        {
+          name: '`!help`',
+          value: 'Gets command list',
+        }
+      );
+
+    message.channel.send(embed);
   });
 });
 
